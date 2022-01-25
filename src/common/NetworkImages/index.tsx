818751455eb3978d06a2da8f59e45035a5fb10c0
Devang <debusharma1999@gmail.com>
2022-01-26 01:14:37 +0530

    timeline ui responsive

--- src/common/NetworkImages/index.tsx
+++ src/common/NetworkImages/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./index.css";
 function NetworkImage(props: any) {
   return (
-    <div>
+    <div className="nid219NetworkDiv">
       <img className="nii219NetworkImage" src={props.url} />
     </div>
   );
